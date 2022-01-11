e149926bad7b1740ba1944d32b379e156aa4b93f
Wai-Phyoe-Aung20 <67585906+Wai-Phyoe-Aung20@users.noreply.github.com>
2022-01-11 10:27:03 +0630

    created by visual studio code
    #Python
    #Netmiko

---
+++ exercise with while loop user attempt.py
@@ -0,0 +1,27 @@
+from netmiko import ConnectHandler
+from getpass import getpass
+import time 
+from datetime import datetime
+
+user_list={'Admin':'pwd-admin'}
+tempt=0
+while tempt<3:
+   username=input("Type your name here:>>")
+   password=input("Type your password here:>>")
+      
+   if password==user_list['Admin']:
+      username=input ('Enter Username:>> ')
+      password=getpass()
+
+      R01={'host':'192.168.114.5','username':username,'password':'admin','device_type':'cisco_ios'}
+      R02={'host':'192.168.114.4','username':username,'password':'admin','device_type':'cisco_ios'}
+      cmd=ConnectHandler(**R01)
+      print (cmd.find_prompt())
+      config_cmd=cmd.send_command('show version')
+      print (config_cmd)
+      break
+   else:
+      print ("Type Again===>")
+      tempt+=1
+ 
+
